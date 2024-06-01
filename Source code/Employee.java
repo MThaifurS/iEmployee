cfd2ea915f978bd9b5624a6c51f99470acc2f1fa
thaifur shahidan <162593776+MThaifurS@users.noreply.github.com>
2024-06-01 13:37:16 +0800

    added file for the source code

---
+++ Source code/Employee.java
@@ -0,0 +1,17 @@
+
+/* This class represents individual employees with attributes like name, ID, 
+leave entitlement, leave list, and profile information. */
+
+import java.util.ArrayList;
+
+
+public class Employee {
+
+    private ArrayList<String> names;
+    private ArrayList<Integer> ids;
+    private ArrayList<Integer> leaveEntitled;
+    private ArrayList<Leave> leaves;
+    private Profile profile;
+
+      
+}
---
+++ Source code/Profile.java
@@ -0,0 +1,20 @@
+
+
+/*This class Contains additional information about an 
+employee, such as department and date joined. */
+public class Profile {
+    
+    private String department;
+    private String dateJoined;
+
+
+    public String getDepartment() {
+        
+        return department;
+    }
+
+    public String getDateJoined() {
+        return dateJoined;
+    }
+    
+}
