3269840c4d45330a75275b9c35a35585ec75f63d
thaifur shahidan <162593776+MThaifurS@users.noreply.github.com>
2024-06-04 21:46:27 +0800

    Update Leave.java

--- Source code/Leave.java
+++ Source code/Leave.java
@@ -1,4 +1,4 @@
-//coded by muhammad thaifur
+//coded by Thaifur(24000641), Adam Ali(24000180), Dwayne(24000257), Syabil(24001125)
 public class Leave {
     private String startDate;
     private String endDate;
