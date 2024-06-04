9119dffe5559721da4600b0626e8d3c509c7840f
thaifur shahidan <162593776+MThaifurS@users.noreply.github.com>
2024-06-04 21:47:21 +0800

    Update Profile.java

--- Source code/Profile.java
+++ Source code/Profile.java
@@ -1,7 +1,9 @@
 
-
 /*This class Contains additional information about an 
 employee, such as department and date joined. */
+
+//coded by Thaifur(24000641), Adam Ali(24000180), Dwayne(24000257), Syabil(24001125)
+
 public class Profile {
     
     private String department;
